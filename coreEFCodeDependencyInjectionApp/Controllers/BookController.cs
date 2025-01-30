653541f424709425c115157a259e65cf7d2ab322
bgunwani <bhawna.gunwani@gmail.com>
2025-01-30 16:30:57 +0530

    Push 9

---
+++ coreEFCodeDependencyInjectionApp/Controllers/BookController.cs
@@ -0,0 +1,95 @@
+﻿using coreEFCodeDependencyInjectionApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreEFCodeDependencyInjectionApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            if(book == null)
+            {
+                return NotFound();
+            }    
+            return View(book);
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            try
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
